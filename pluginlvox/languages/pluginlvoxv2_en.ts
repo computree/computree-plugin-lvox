99e9dfffbaf2398b3dfc04b9be78ff3959f8707b
Alexandre Piboule <alexandre.piboule@onf.fr>
2014-10-14 08:58:37 +0000

    ts
    git-svn-id: http://rdinnovation.onf.fr/svn/plugin-lvox@53 f1561d03-89af-ab4a-a97c-2bf9dc1f41eb

---
+++ pluginlvox/languages/pluginlvoxv2_en.ts
@@ -0,0 +1,223 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>LVOX_StepCombineDensityGrids</name>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="44"/>
+        <source>Combinaison de grilles 3D de différentes points de vues</source>
+        <translation>Combination of 3D grids from differents points of views</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="58"/>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="86"/>
+        <source>Grilles</source>
+        <translation>Grids</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="63"/>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="87"/>
+        <source>hits</source>
+        <translation>hits</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="66"/>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="88"/>
+        <source>theorical</source>
+        <translation>theorical</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="69"/>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="89"/>
+        <source>before</source>
+        <translation>before</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="72"/>
+        <source>density</source>
+        <translation>density</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="84"/>
+        <source>Grilles combinées</source>
+        <translation>Combined grids</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="90"/>
+        <source>density - %1</source>
+        <translation>density - %1</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="91"/>
+        <source>scanId</source>
+        <translation>scanId</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="138"/>
+        <source>    -&gt; (nt - nb) minimum</source>
+        <translation>    -&gt; (nt - nb) minimum</translation>
+    </message>
+</context>
+<context>
+    <name>LVOX_StepComputeLvoxGrids</name>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="80"/>
+        <source>Calcul de grilles 3D densités corrigées</source>
+        <translation>Computing of corrected 3D grids</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="91"/>
+        <source>Scène(s)</source>
+        <translation>Scene(s)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="94"/>
+        <source>Scan</source>
+        <translation>Scan</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="95"/>
+        <source>Scène</source>
+        <translation>Scene</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="96"/>
+        <source>Scanner</source>
+        <translation>Scanner</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="99"/>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="102"/>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="103"/>
+        <source>Grille de référence</source>
+        <translation>Reference grid</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="99"/>
+        <source>OPTIONNEL</source>
+        <translation>OPTIONNAL</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="114"/>
+        <source>Hits</source>
+        <translation>Hits</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="115"/>
+        <source>Theorical</source>
+        <translation>Theorical</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="116"/>
+        <source>Before</source>
+        <translation>before</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="117"/>
+        <source>Density</source>
+        <translation>density</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="121"/>
+        <source>DeltaIn</source>
+        <translation>DeltaIn</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="122"/>
+        <source>DeltaOut</source>
+        <translation>DeltaOut</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="123"/>
+        <source>DeltaTheorical</source>
+        <translation>DeltaTheorical</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="124"/>
+        <source>DeltaBefore</source>
+        <translation>DeltaBefore</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="136"/>
+        <source>Use reference grid resolution</source>
+        <translation>Use reference grid resolution</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="136"/>
+        <source>(if reference grid has been specified...)</source>
+        <translation>(if reference grid has been specified...)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="137"/>
+        <source>If not : resolution of the grids</source>
+        <translation>If not : resolution of the grids</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="137"/>
+        <source>meters</source>
+        <translation>meters</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="138"/>
+        <source>Minimum number of effective ray in a voxel to take it into account</source>
+        <translation>Minimum number of effective ray in a voxel to take it into account</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="139"/>
+        <source>Compute Distances</source>
+        <translation>Compute Distances</translation>
+    </message>
+</context>
+<context>
+    <name>LVOX_StepLoadInFile</name>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="49"/>
+        <source>Chargement de nuages de points / fichier .in</source>
+        <translation>Loading point clouds / .in file</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="69"/>
+        <source>IndividualScenes</source>
+        <translation>Individual Scenes</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="72"/>
+        <source>IndividualScene</source>
+        <translation>Individual Scene</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="73"/>
+        <source>IndividualSceneColors</source>
+        <translation>Individual Scene Colors</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="74"/>
+        <source>IndividualSceneIntensity</source>
+        <translation>Individual Scene Intensity</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="75"/>
+        <source>ScanPosition</source>
+        <translation>Scan Position</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="78"/>
+        <location filename="../step/lvox_steploadinfile.cpp" line="81"/>
+        <source>MergedScene</source>
+        <translation>Merged Scene</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="142"/>
+        <source>File reader created</source>
+        <translation>File reader created</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="150"/>
+        <source>Wrong file extension</source>
+        <translation>Wrong file extension</translation>
+    </message>
+</context>
+</TS>
---
+++ pluginlvox/languages/pluginlvoxv2_fr.ts
@@ -0,0 +1,223 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>LVOX_StepCombineDensityGrids</name>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="44"/>
+        <source>Combinaison de grilles 3D de différentes points de vues</source>
+        <translation>Combinaison de grilles 3D de différentes points de vues</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="58"/>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="86"/>
+        <source>Grilles</source>
+        <translation>Grilles</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="63"/>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="87"/>
+        <source>hits</source>
+        <translation>hits (retours)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="66"/>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="88"/>
+        <source>theorical</source>
+        <translation>theorical (théoriques)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="69"/>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="89"/>
+        <source>before</source>
+        <translation>before (intercéptés avant)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="72"/>
+        <source>density</source>
+        <translation>densité</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="84"/>
+        <source>Grilles combinées</source>
+        <translation>Grilles combinées</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="90"/>
+        <source>density - %1</source>
+        <translation>densité - %1</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="91"/>
+        <source>scanId</source>
+        <translation>Id scan</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcombinedensitygrids.cpp" line="138"/>
+        <source>    -&gt; (nt - nb) minimum</source>
+        <translation>    -&gt; (nt - nb) minimum</translation>
+    </message>
+</context>
+<context>
+    <name>LVOX_StepComputeLvoxGrids</name>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="80"/>
+        <source>Calcul de grilles 3D densités corrigées</source>
+        <translation>Calcul de grilles 3D densités corrigées</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="91"/>
+        <source>Scène(s)</source>
+        <translation>Scène(s)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="94"/>
+        <source>Scan</source>
+        <translation>Scan</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="95"/>
+        <source>Scène</source>
+        <translation>Scène</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="96"/>
+        <source>Scanner</source>
+        <translation>Scanner</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="99"/>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="102"/>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="103"/>
+        <source>Grille de référence</source>
+        <translation>Grille de référence</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="99"/>
+        <source>OPTIONNEL</source>
+        <translation>OPTIONNEL</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="114"/>
+        <source>Hits</source>
+        <translation>hits (retours)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="115"/>
+        <source>Theorical</source>
+        <translation>theorical (théoriques)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="116"/>
+        <source>Before</source>
+        <translation>before (intercéptés avant)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="117"/>
+        <source>Density</source>
+        <translation>densité</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="121"/>
+        <source>DeltaIn</source>
+        <translation>DeltaIn</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="122"/>
+        <source>DeltaOut</source>
+        <translation>DeltaOut</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="123"/>
+        <source>DeltaTheorical</source>
+        <translation>DeltaTheorical</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="124"/>
+        <source>DeltaBefore</source>
+        <translation>DeltaBefore</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="136"/>
+        <source>Use reference grid resolution</source>
+        <translation>Utiliser la résolution de la grille de référence</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="136"/>
+        <source>(if reference grid has been specified...)</source>
+        <translation>(si une grille de référence a été séléctionnée)</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="137"/>
+        <source>If not : resolution of the grids</source>
+        <translation>Sinon : résolution des grilles</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="137"/>
+        <source>meters</source>
+        <translation>mètres</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="138"/>
+        <source>Minimum number of effective ray in a voxel to take it into account</source>
+        <translation>Nombre minimum de rayons effectifs dans un voxel pour qu&apos;ils soient pris en compte</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_stepcomputelvoxgrids.cpp" line="139"/>
+        <source>Compute Distances</source>
+        <translation>Calcul des distances</translation>
+    </message>
+</context>
+<context>
+    <name>LVOX_StepLoadInFile</name>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="49"/>
+        <source>Chargement de nuages de points / fichier .in</source>
+        <translation>Chargement de nuages de points / fichier .in</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="69"/>
+        <source>IndividualScenes</source>
+        <translation>Scènes individuelles</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="72"/>
+        <source>IndividualScene</source>
+        <translation>Scène individuelle</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="73"/>
+        <source>IndividualSceneColors</source>
+        <translation>Couleurs de scène individuelle</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="74"/>
+        <source>IndividualSceneIntensity</source>
+        <translation>Intensité de scène individuelle</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="75"/>
+        <source>ScanPosition</source>
+        <translation>Position de scan</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="78"/>
+        <location filename="../step/lvox_steploadinfile.cpp" line="81"/>
+        <source>MergedScene</source>
+        <translation>Scène fusionnée</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="142"/>
+        <source>File reader created</source>
+        <translation>Lecteur de fichier créé</translation>
+    </message>
+    <message>
+        <location filename="../step/lvox_steploadinfile.cpp" line="150"/>
+        <source>Wrong file extension</source>
+        <translation>Mauvaise extension de fichier</translation>
+    </message>
+</context>
+</TS>
